b801ccdf11297f13b0452fb1e5c450800f07c1e2
TheEmeraldHunter <joeygujral@gmail.com>
2017-09-17 21:39:14 +0300

    i ow

--- Party Rock App/MainVC.swift
+++ Party Rock App/MainVC.swift
@@ -19,8 +19,24 @@ class MainVC: UIViewController, UITableViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) -> UITableViewCell {
+  
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "Party Cell", for: indexPath) as? PartyCell{
+        
+        }
+        
+        return UITableViewCell()
+        
     }
 
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return PartyRocks.count
+    }
 
 
 }
